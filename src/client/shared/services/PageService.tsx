3b497ad8d62abd67b14807c52038426aec886071
ameliagranular <ameliacatalano@granular.ag>
2018-05-10 11:51:29 -0700

    .

--- src/client/shared/services/PageService.tsx
+++ src/client/shared/services/PageService.tsx
@@ -101,12 +101,4 @@ export function renderPage(
         );
     }
 
-    /* dev-block:start */
-    // Enable live reload for development
-    document.write(
-        '<script src="http://' +
-        (location.host || 'localhost').split(':')[0] +
-        ':35729/livereload.js?snipver=1"></script>'
-    );
-    /* dev-block:end */
 }
