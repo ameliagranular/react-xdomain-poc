13903cb19ce6d7616e8f481263e79b949a7bd558
ameliagranular <ameliacatalano@granular.ag>
2018-05-11 09:14:42 -0700

    add proxy

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var proxy = require('express-http-proxy');
 var app = express();
 
 app.use(express.static(__dirname +'./../../')); //serves the index.html
+
+app.use('/ajax/shared/get_organization_users', proxy('local.granular.test:6543/ajax/shared/get_organization_users'));
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
